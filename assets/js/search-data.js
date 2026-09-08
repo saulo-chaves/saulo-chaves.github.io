624eb352a73ecf411861ae23007fd1db52a61d24
saulo-chaves <101746579+saulo-chaves@users.noreply.github.com>
2026-09-08 22:10:25 +0000

    Deploying to gh-pages from @ saulo-chaves/saulo-chaves.github.io@dd9b79ea1a99281235664a6d827ca0b9d988fa9e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -84,6 +84,11 @@ ninja.data = [{
           description: "A three-day online short-course on genomic selection given by me and organized by GVENCK (ESALQ). Find the data used in the codes in https://github.com/saulo-chaves/GS_GVENCK",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_GS_GVENCK/";
+            },},{id: "projects-genetic-competition-mixed-models-using-gencomp",
+          title: 'Genetic competition mixed models using gencomp',
+          description: "A short-course on mixed models applied to genetic competition analysis using the gencomp package",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/5_gencomp/";
             },},{
         id: 'social-email',
         title: 'email',
